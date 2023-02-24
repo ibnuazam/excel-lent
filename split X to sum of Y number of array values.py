71b5c8d07a35a357fcc21243719b95aa82e6c20b
Ibnu Azam A <azam.alfanibnu@gmail.com>
2023-02-24 08:21:21 +0700

    24 Februari 2023

--- split X to sum of Y number of array values.py
+++ split X to sum of Y number of array values.py
@@ -1,17 +1,17 @@
 import random
 
 # insert a value or array of values to split
-A = [80, 88, 98, 98, 80, 80, 90, 98, 98, 80, 80, 98, 80, 80, 98, 98, 80, 98, 80, 98, 80, 80, 75, 80, 80, 98, 98, 80, 98, 75, 80, 80]
+A = [75, 75, 75, 75, 75, 75, 70, 75, 80, 75, 70, 70, 70, 75, 75, 70, 80, 80, 75, 75, 70, 75, 75, 75, 70, 75, 75, 80, 75, 85, 75, 70]
 
 # function to split value to sum of Y random values
 # change split array length to number of values you want to split to
 # change Y in range(Y) to match with split array length
 # change a,b in random.randint(a, b) to set the range (lowest-highest) of random values to be generated
 def split_value(value):
-    split = [0, 0, 0, 0, 0]
+    split = [0, 0, 0, 0]
     while sum(split) != value:
-        for i in range(5):
-            split[i] = random.randint(1, 20)
+        for i in range(4):
+            split[i] = random.randint(1, 25)
     return split
 
 # print sorted array of split values
